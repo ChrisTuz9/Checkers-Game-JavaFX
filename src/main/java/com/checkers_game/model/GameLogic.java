b2cad063e4b36726e26a58a3495248669ce12492
Kšyštof Tužik <krzysztof2003t@gmail.com>
2024-11-08 16:19:20 +0200

    Add kings move

--- src/main/java/com/checkers_game/model/GameLogic.java
+++ src/main/java/com/checkers_game/model/GameLogic.java
@@ -18,14 +18,19 @@ public class GameLogic {
         int rowDiff = Math.abs(from.getRow() - to.getRow());
         int colDiff = Math.abs(from.getCol() - to.getCol());
 
+        if(rowDiff != colDiff)
+            return false;
+
+        if(from.getPiece().isKing())
+            return true;
         return rowDiff == 1 && colDiff == 1;
     }
 
     public void checkPromotion(Tile toTile) {
         Piece piece = toTile.getPiece();
         if (piece != null) {
-            if ((piece.getColor() == PieceColor.WHITE && toTile.getRow() == 0) ||
-                    (piece.getColor() == PieceColor.BLACK && toTile.getRow() == 7)) {
+            if ((piece.getColor() == board.getPlayerColor() && toTile.getRow() == 0) ||
+                    (piece.getColor() == board.getOpponentColor() && toTile.getRow() == board.getSIZE() - 1)) {
                 piece.promoteToKing();
             }
         }
