41a70fe8f72dfaabfc0696fd0c00b494739e7013
Kšyštof Tužik <krzysztof2003t@gmail.com>
2024-11-10 20:59:47 +0200

    Add start panel

--- src/main/java/com/checkers_game/Main.java
+++ src/main/java/com/checkers_game/Main.java
@@ -5,23 +5,66 @@ import com.checkers_game.model.Board;
 import com.checkers_game.model.enums.PieceColor;
 import com.checkers_game.view.BoardView;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    private PieceColor selectedColor = PieceColor.WHITE;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
-        Board board = new Board(PieceColor.WHITE);
+    public void start(Stage primaryStage) {
+        showStartScreen(primaryStage);
+    }
+
+    private void showStartScreen(Stage stage) {
+        VBox startLayout = new VBox(20);
+        startLayout.setPadding(new Insets(20));
+        startLayout.setAlignment(Pos.CENTER);
+
+        Text titleText = new Text("Checkers");
+        titleText.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+
+        Label instructionLabel = new Label("Choose a color");
+        instructionLabel.setFont(Font.font("Arial", 16));
+
+        ChoiceBox<PieceColor> colorChoiceBox = new ChoiceBox<>();
+        colorChoiceBox.getItems().addAll(PieceColor.WHITE, PieceColor.BLACK);
+        colorChoiceBox.setValue(PieceColor.WHITE);
+
+        Button startButton = new Button("Start");
+        startButton.setOnAction(e -> {
+            selectedColor = colorChoiceBox.getValue();
+            showGameScreen(stage);
+        });
+
+        startLayout.getChildren().addAll(titleText, instructionLabel, colorChoiceBox, startButton);
+        Scene startScene = new Scene(startLayout, 409, 409);
+        stage.setTitle("Checkers");
+        stage.setScene(startScene);
+        stage.show();
+    }
+
+    private void showGameScreen(Stage stage) {
+        Board board = new Board(selectedColor);
         BoardView boardView = new BoardView(board);
         GameController gameController = new GameController(board, boardView);
 
-        Scene scene = new Scene(boardView.getGridPane(), 409, 409);
-        stage.setTitle("Checkers");
-        stage.setScene(scene);
+        Scene gameScene = new Scene(boardView.getGridPane(), 409, 409);
+        stage.setScene(gameScene);
         stage.show();
     }
 }
--- src/main/java/com/checkers_game/controllers/GameController.java
+++ src/main/java/com/checkers_game/controllers/GameController.java
@@ -116,5 +116,6 @@ public class GameController {
         alert.setContentText(message);
 
         alert.showAndWait();
+        System.exit(0);
     }
 }
